7e4b32ddb1c08d2270a07f35768a48e4bca17621
Konstantin Likhter <konstantin.likhter@intel.com>
2014-11-13 10:13:05 -0800

    XdkRunEmulatorCommand + XdkConfigureCommand

--- XDK/xdk_run_emulator.py
+++ XDK/xdk_run_emulator.py
@@ -1,5 +1,6 @@
 import sublime, sublime_plugin
 import XDK.core
+from XDK.core import MSGS
 
 #class ExampleCommand(sublime_plugin.TextCommand):
 #	def run(self, edit):
@@ -7,7 +8,25 @@ import XDK.core
 
 class XdkRunEmulatorCommand(sublime_plugin.TextCommand, XDK.core.XDKPluginCore):
 	def run(self, edit):
-		self.prepare()
+		if not self.prepare(): return
+
+		try:
+			folder = self.view.window().folders()[0]
+		except:
+			sublime.error_message(MSGS['CAN_NOT_GET_FOLDER'])
+			return
+
+		filename = self.view.file_name();
+		self.invoke_command({
+			'action': 'run_emulator',
+			'folder': folder,
+			'filename': filename
+		})
+
+class XdkConfigureCommand(sublime_plugin.TextCommand, XDK.core.XDKPluginCore):
+	def run(self, edit):
+		self.show_configuration_prompt()
+
 
 
 
