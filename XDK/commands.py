87ac0a795b9b1c6337551904207a96728a9af237
Konstantin Likhter <konstantin.likhter@intel.com>
2015-02-02 16:55:54 -0800

    fix HTML-8518

--- XDK/commands.py
+++ XDK/commands.py
@@ -128,11 +128,11 @@ class XdkShowAboutCommand(sublime_plugin.ApplicationCommand):
 	def run(self):
 		sublime.message_dialog(u"""Intel® XDK HTML5 Cross-platform Development Tool enables developers to easily design, test, debug, and build HTML5 web and hybrid apps across multiple app stores and form factor devices.  
 
-The Intel XDK plugin for Sublime Text* allows you to drive your HTML5 app emulation, testing, profiling, and builds from within Sublime Text; you just need to set up your project within the Intel XDK first.  
+The Intel® XDK plugin for Sublime Text* allows you to drive your HTML5 app emulation, testing, profiling, and builds from within Sublime Text; you just need to set up your project within the Intel® XDK first.  
 
-See XXX for more information about the Intel XDK Sublime Text* plugin.  
+See XXX for more information about the Intel® XDK Sublime Text* plugin.  
 
-Please see http://xdk.intel.com for more information about the Intel XDK and for support.
+Please see http://xdk.intel.com for more information about the Intel® XDK and for support.
 
 *Other names and brands may be claimed as the property of others.   
 Copyright (c) 2013-2014 Intel Corporation. All Rights Reserved.
