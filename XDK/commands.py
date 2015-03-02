dd1b9a172cf548e270ce3582863115bcb0849b66
Konstantin Likhter <konstantin.likhter@intel.com>
2015-03-02 14:19:39 -0800

    Universal core import + packages.json for PackageControl

--- XDK/commands.py
+++ XDK/commands.py
@@ -16,11 +16,13 @@ import sublime, sublime_plugin
 import os
 import sys
 
-if sublime.version() < '3':
-	import XDKCore
-else:
-	import XDK.XDKCore as XDKCore
+__file__ = os.path.normpath(os.path.abspath(__file__));
+__path__ = os.path.dirname(__file__)
 
+if __path__ not in sys.path:
+	sys.path.insert(0, __path__)
+
+import XDKCore
 
 class XdkCommand_(sublime_plugin.TextCommand, XDKCore.XDKPluginCore):
 	def run(self, edit):
